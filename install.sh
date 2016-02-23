5906484247e44828ba92d4355ee68adc05238790
Ivan Mrvelj <sk8raid@gmail.com>
2016-02-23 19:36:03 +0100

    Fixing stuff, added gitconfig

--- install.sh
+++ install.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-dir=/storage/dev/projects/dotfiles                    # dotfiles directory
-olddir=/storage/dev/projects/dotfiles/old
-files="zshrc Xresources tmux.conf"    # list of files/folders to symlink in homedir
+dir=/storage/dev/projects/dotfiles
+olddir=/home/rejd/dotfiles_old
+files="zshrc Xresources tmux.conf gitconfig"
 
-# create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
 
-# change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -sf $dir/$file ~/.$file
 done
