dcb74cb1f799b993cd1b910122572a6e7851a346
Ivan Mrvelj <imrvelj@protonmail.com>
2020-10-28 23:47:21 +0100

    feat: revert bar changes

--- polybar/.config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# Terminate already running bar instances
-killall -q polybar
-# Wait until the processes have been shut down
-while pidof polybar >/dev/null; do sleep 1; done
-
-polybar dummy &
-polybar default &
-
