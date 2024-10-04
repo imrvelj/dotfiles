7f63248975f56e8f53fba28506cddf9d90ea53b0
Ivan Mrvelj <imrvelj@protonmail.com>
2024-10-04 20:31:51 +0200

    fix(nvim): cleaner lsp code

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,46 +1,79 @@
 return {
   {
     'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    lazy = true,
-    config = false,
-    init = function()
-      vim.g.lsp_zero_extend_cmp = 0
-      vim.g.lsp_zero_extend_lspconfig = 0
-    end,
-  },
-
-  {
-    'williamboman/mason.nvim',
-    lazy = false,
-    config = true,
-  },
-
-  -- Autocompletion
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
+    branch = 'v4.x',
     dependencies = {
-      { 'L3MON4D3/LuaSnip' },
+      'neovim/nvim-lspconfig',
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-nvim-lsp',
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
     },
     config = function()
       local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_cmp()
 
+      local lsp_attach = function(client, bufnr)
+        local opts = { buffer = bufnr }
+
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+      end
+
+      lsp_zero.extend_lspconfig({
+        sign_text = true,
+        lsp_attach = lsp_attach,
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+      })
+
+      -- Mason
+      require('mason').setup()
+      require('mason-lspconfig').setup({
+        ensure_installed = { 'eslint', 'ts_ls', 'denols', 'tailwindcss', 'prismals', 'elixirls' },
+        handlers = {
+          lsp_zero.default_setup,
+          denols = function()
+            local nvim_lsp = require('lspconfig')
+            nvim_lsp.denols.setup({
+              root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+            })
+          end,
+          ts_ls = function()
+            local nvim_lsp = require('lspconfig')
+            nvim_lsp.ts_ls.setup({
+              single_file_support = false,
+              root_dir = nvim_lsp.util.root_pattern("package.json"),
+            })
+          end
+        }
+      })
+
+      -- Cmp
       local cmp = require('cmp')
-      local cmp_action = lsp_zero.cmp_action()
 
       cmp.setup({
-        formatting = lsp_zero.cmp_format(),
+        sources = {
+          { name = 'nvim_lsp' },
+        },
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
+        },
         mapping = cmp.mapping.preset.insert({
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
           ['<Tab>'] = cmp.mapping.confirm({ select = true }),
           ['<C-u>'] = cmp.mapping.scroll_docs(-4),
           ['<C-d>'] = cmp.mapping.scroll_docs(4),
-          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-        })
+        }),
       })
     end
   },
@@ -61,49 +94,6 @@ return {
         indent = { enable = true },
       })
     end,
-  },
-
-  -- LSP
-  {
-    'neovim/nvim-lspconfig',
-    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-    event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = {
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'williamboman/mason-lspconfig.nvim' },
-    },
-    config = function()
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_lspconfig()
-
-      lsp_zero.on_attach(function(client, bufnr)
-        lsp_zero.default_keymaps({ buffer = bufnr })
-      end)
-
-      require('mason-lspconfig').setup({
-        ensure_installed = { 'eslint', 'ts_ls', 'denols', 'tailwindcss', 'prismals', 'elixirls' },
-        handlers = {
-          lsp_zero.default_setup,
-          lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-          end,
-          denols = function()
-            local nvim_lsp = require('lspconfig')
-            nvim_lsp.denols.setup({
-              root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
-            })
-          end,
-          ts_ls = function()
-            local nvim_lsp = require('lspconfig')
-            nvim_lsp.ts_ls.setup({
-              single_file_support = false,
-              root_dir = nvim_lsp.util.root_pattern("package.json"),
-            })
-          end
-        }
-      })
-    end
   },
 
   -- Formatting
